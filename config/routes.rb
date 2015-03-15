7ee945f5a57d4ea7c3554bef9c6ef41e1eeb019d
Natnicha Leung-on <5510613424@student.tu.ac.th>
2015-03-15 21:13:47 +0000

    Test on haroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 TermProject::Application.routes.draw do
   root to: 'about#index'
-    get "/", :to => 'about#index' , :as => 'about'
-    get "about/", :to => 'about#index' , :as => 'about'
-    get "about/index", :to => 'about#index' , :as => 'about'
+  get "about/", :to => 'about#index' , :as => 'about'
+  get "about/index", :to => 'about#index' , :as => 'about'
 end
