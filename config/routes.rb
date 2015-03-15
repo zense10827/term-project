d84c81e21376540d1e33084bd6ca38442d3c788d
Natnicha Leung-on <5510613424@student.tu.ac.th>
2015-03-15 21:15:45 +0000

    Test on haroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 TermProject::Application.routes.draw do
+  resources :about
   root to: 'about#index'
   get "about/", :to => 'about#index' , :as => 'about'
   get "about/index", :to => 'about#index' , :as => 'about'
