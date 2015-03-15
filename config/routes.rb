6602453625217b43597b8ef7b534b6af5591ac83
Natnicha Leung-on <5510613424@student.tu.ac.th>
2015-03-15 21:38:26 +0000

    Test on haroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
-Rails.application.routes.draw do
+TermProject::Application.routes.draw do
   resources :about
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-
-  root 'about#index'
+  root :to => redirect('/about')
 end
