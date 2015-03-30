ca2476d64f254dbcedc08a5d7d0835d07d99fdca
Natnicha Leung-on <5510613424@student.tu.ac.th>
2015-03-30 02:27:47 +0000

    iteration 1-3

--- spec/controllers/course_controller_spec.rb
+++ spec/controllers/course_controller_spec.rb
@@ -1,24 +1,29 @@
 require 'rails_helper'
 require 'spec_helper'
-RSpec.describe CourseController, :type => :controller do
 
-  describe 'create' do
-    it 'should add a new course' do
-      post create
-      flash[:notice].should_not be_nil
-    end
-  end
-  describe 'index'
+RSpec.describe CourseController, :type => :controller do
+  
+  describe 'index' do
     it 'show all course' do
       get :index
       response.should render_template('index')
     end
   end
+  
+  describe 'create' do
+    it 'should add a new course' do
+      post :create, {:id => "1"}
+      flash[:notice].should_not be_nil
+    end
+  end
+  
   describe 'destroy' do
-    it 'should destroy a course' do
+		it 'should destroy a movie' do
       m = mock(Course, :id => "10")
       Course.stub!(:find).with("10").and_return(m)
       m.should_receive(:destroy)
       delete :destroy, {:id => "10"}
     end
-  end
+	end
+  
+end
